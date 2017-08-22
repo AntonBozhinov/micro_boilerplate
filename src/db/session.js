e374cdc090355ddd9b7935705bc2c163052d18a2
Anton Bozhinov <Anton.Bozhinov@Softwaregroup-bg.com>
2017-08-22 11:11:49 +0300

    fix driver connection

--- src/db/session.js
+++ src/db/session.js
@@ -1,5 +1,5 @@
 const neo4j = require('neo4j-driver').v1;
-const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '123'));
+const driver = neo4j.driver(`bolt://${process.env.DB_HOST || 'localhost'}`, neo4j.auth.basic('neo4j', process.env.DB_PASS));
 const session = driver.session();
 
 process.on('exit', () => {
--- tests/endpoint.test.js
+++ tests/endpoint.test.js
@@ -1,6 +1,16 @@
 import test from 'ava';
 import request from 'supertest';
 import server from '../src';
+import Requester from '../src/db';
+import session from '../src/db/session';
+
+const db = new Requester(session);
+
+test.before('DB CLEAN UP', async t => {
+    t.plan(1);
+    const res = await db.createNode('Person');
+    t.is(true, !!res)
+})
 
 test('GET / : hello from server', async t => {
     t.plan(1);
